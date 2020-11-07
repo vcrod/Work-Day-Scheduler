3a19fed25a84c2a0beb339400325b51bbe1326b7
yluego <vcrod79@gmail.com>
2020-11-06 19:57:48 -0700

    changed textarea rows from 10 to 8

--- Develop/script.js
+++ Develop/script.js
@@ -30,5 +30,13 @@ $(document).ready(function () {
   color();
   // get info local storage - repeat for hour blocks
   $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
 })
